db969faa7ceb664baa8b792e0c66a64d9217a3fb
Carolina <carovel2608@gmail.com>
2024-03-06 19:36:03 -0500

    feat: filters file

--- todo-list/src/components/Filters/Filters.jsx
+++ todo-list/src/components/Filters/Filters.jsx
@@ -1,7 +1,16 @@
 export function Filters() {
     return(
       <>
-  
+      <div className="filter-container">
+        <input type="radio" id="1" value="all" />
+        <label for="all">All</label>
+
+        <input type="radio" id="2" value="completed" />
+        <label for="completed">Completed</label>
+
+        <input type="radio" id="3" value="pending" />
+        <label for="pending">Pending</label>
+      </div>
       </>
     )
 }
