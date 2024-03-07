ab731ff02f72a9e17a60655b581944ff36b2a2fd
Carolina <carovel2608@gmail.com>
2024-03-06 19:36:25 -0500

    feat: files edited

--- todo-list/src/components/Form/Form.jsx
+++ todo-list/src/components/Form/Form.jsx
@@ -1,7 +1,15 @@
 export function Form() {
     return(
-      <>
-  
+      <>     
+      <div className="form-container">
+        <h1> To do List </h1> 
+        <input
+        type="text"
+        placeholder='Add a task'
+        />
+
+        <button> Add </button>
+      </div>
       </>
     )
 }
--- todo-list/src/components/List/List.jsx
+++ todo-list/src/components/List/List.jsx
@@ -1,7 +1,9 @@
 export function List() {
     return(
       <>
-  
+      <div className="list-container">
+        <ul> Una lista </ul>
+      </div>
       </>
     )
 }
